c4eed3004c53b7a8334152882e4485d833ecfce0
Itzcoatl <itzcoatl90@gmail.com>
2013-12-02 13:49:25 -0600

    Este será el proyecto, es una carátula apenas
    Para la materia de Tópicos de Gestión de seguridad informática

---
+++ GestionDeSeguridad/src/gestiondeseguridad/GestionDeSeguridad.java
@@ -0,0 +1,12 @@
+package gestiondeseguridad;
+
+/**
+ *
+ * @author Itzcoatl90
+ */
+public class GestionDeSeguridad {
+
+    public static void main(String[] args) {
+        System.out.println("Hola mundo! Ahora a disfrutar de este espacio virtual.");
+    }
+}
