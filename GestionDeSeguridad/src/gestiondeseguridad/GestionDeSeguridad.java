37311eda4658ef058046874447bbc1c9a9ff1e43
Mario <Mario@mbp-de-mario.lan>
2013-12-03 00:14:49 -0600

    Agrego línea de prueba.

--- GestionDeSeguridad/src/gestiondeseguridad/GestionDeSeguridad.java
+++ GestionDeSeguridad/src/gestiondeseguridad/GestionDeSeguridad.java
@@ -8,5 +8,6 @@ public class GestionDeSeguridad {
 
     public static void main(String[] args) {
         System.out.println("Hola mundo! Ahora a disfrutar de este espacio virtual.");
+        System.out.println("Probando GIT, linea agregada por Mario Sotelo.");
     }
 }
