c8445e87fece01adbecee51870eb14138585bb5e
danyraziel <rowley_raziel@hotmail.com>
2013-12-03 00:32:22 -0600

    Prueba Dniel Zam

--- GestionDeSeguridad/src/gestiondeseguridad/GestionDeSeguridad.java
+++ GestionDeSeguridad/src/gestiondeseguridad/GestionDeSeguridad.java
@@ -9,5 +9,6 @@ public class GestionDeSeguridad {
     public static void main(String[] args) {
         System.out.println("Hola mundo! Ahora a disfrutar de este espacio virtual.");
         System.out.println("Probando GIT, linea agregada por Mario Sotelo.");
+        System.out.println("Prueba Daniel Zam");
     }
 }
