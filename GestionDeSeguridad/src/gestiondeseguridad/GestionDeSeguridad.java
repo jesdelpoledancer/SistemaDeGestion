4aab49485cf13ffd31c7b7447de3613ad6e9d328
Toshiba <Toshiba@Toshiba-PC>
2013-12-03 01:09:16 -0600

    Prueba Giovanni

--- GestionDeSeguridad/src/gestiondeseguridad/GestionDeSeguridad.java
+++ GestionDeSeguridad/src/gestiondeseguridad/GestionDeSeguridad.java
@@ -10,5 +10,6 @@ public class GestionDeSeguridad {
         System.out.println("Hola mundo! Ahora a disfrutar de este espacio virtual.");
         System.out.println("Probando GIT, linea agregada por Mario Sotelo.");
         System.out.println("Prueba Daniel Zam");
+        System.out.println("Gio prueba ");
     }
 }
