39e5d267b6e0057707834d5590b5e155f789bc8c
Toshiba <Toshiba@Toshiba-PC>
2013-12-06 01:18:14 -0600

    Lector Escritor De Entidades Directas con un ejemplode escritura(comentado) y uno de lectura. La info se almacena en DirEnt.sg

--- GestionDeSeguridad/src/gestiondeseguridad/GestionDeSeguridad.java
+++ GestionDeSeguridad/src/gestiondeseguridad/GestionDeSeguridad.java
@@ -1,6 +1,13 @@
 package gestiondeseguridad;
 
 import Controladores.Mediator;
+import Estructuras.Riesgo;
+import Estructuras.Tarea;
+import Modelos.EntityDB;
+import Modelos.LectorEscritorDeEntidades;
+import java.sql.Time;
+import java.util.ArrayList;
+import java.util.Date;
 
 /**
  *
@@ -14,7 +21,67 @@ public class GestionDeSeguridad {
         System.out.println("Prueba Daniel Zam");
         System.out.println("Gio prueba ");
         System.out.println("ya esta prueba Joel ");
-        
+        /*
+         * Pruebas Lectura y escritura
+         * Escritura
+        */
+//        EntityDB db=EntityDB.getInstance();
+//        Tarea t = new Tarea();
+//        t.setNombre("Tarea1");
+//        t.setDescripcion("Descripcion 1");
+//        db.agregarTarea(t);
+//        t = new Tarea();
+//        t.setNombre("Tarea2");
+//        t.setDescripcion("Descripcion 2");
+//        db.agregarTarea(t);
+//        t = new Tarea();
+//        t.setNombre("Tarea3");
+//        t.setDescripcion("Descripcion 3");
+//        db.agregarTarea(t);
+//        t = new Tarea();
+//        t.setNombre("Tarea4");
+//        t.setDescripcion("Descripcion 4");
+//        db.agregarTarea(t);
+//        Riesgo r=new Riesgo();
+//        r.setActivo("Ac1");
+//        r.setAmenaza("Am1");
+//        r.setImpacto(1);
+//        r.setOcurrencia(1);
+//        r.setProximoMonitoreo(new Date());
+//        r.setResultado("r1");
+//        r.setTiempoDeMonitoreo(new Time(1));
+//        r.setTratamiento("tr1");
+//        db.agregarRiesgo(r);
+//         r=new Riesgo();
+//        r.setActivo("Ac2");
+//        r.setAmenaza("Am2");
+//        r.setImpacto(2);
+//        r.setOcurrencia(2);
+//        r.setProximoMonitoreo(new Date());
+//        r.setResultado("r2");
+//        r.setTiempoDeMonitoreo(new Time(2));
+//        r.setTratamiento("tr2");
+//        db.agregarRiesgo(r);
+//        LectorEscritorDeEntidades l= new LectorEscritorDeEntidades();
+//        System.out.println(l.escribirEntidadesDirectas());
+        /*
+         * Pruebas Lectura y escritura
+         * Lectura
+        */
+        LectorEscritorDeEntidades l= new LectorEscritorDeEntidades();
+       l.leerEntidadesDirectas();
+       EntityDB db=EntityDB.getInstance();
+       ArrayList lista=db.getRiesgos();
+        for (Object e : lista) {
+         Riesgo r=(Riesgo)e;
+                System.out.println("Activo:"+r.getActivo()+" Tiempo Monitoreo:"+r.getTiempoDeMonitoreo());   
+        }
+      ArrayList tt=db.getTareas();
+            for (Object e : tt) {
+                Tarea r=(Tarea)e;
+                System.out.println("Tarea:"+r.getNombre()+" Descripcion: "+r.getDescripcion());
+            }
+       
         Mediator m = new Mediator();
     }
 }
